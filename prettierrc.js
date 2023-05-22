f82a55937455855e9d75431909fb54abd93f176c
Artur Kmera <artur.kmera@as-pl.com>
2023-05-22 16:51:15 +0200

    Add phased builds

--- prettierrc.js
+++ prettierrc.js
@@ -1,8 +1,11 @@
 // Documentation for this file: https://prettier.io/en/configuration.html
 module.exports = {
+  plugins: [
+    "./common/autoinstallers/rush-prettier/node_modules/prettier-plugin-packagejson",
+  ],
   // We use a larger print width because Prettier's word-wrapping seems to be tuned
   // for plain JavaScript without type annotations
-  printWidth: 110,
+  printWidth: 120,
 
   // Use .gitattributes to manage newlines
   endOfLine: "auto",
